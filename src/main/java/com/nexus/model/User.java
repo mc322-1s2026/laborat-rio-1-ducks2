e4ff455951757d20b3f8e6abb39c6e9ff62e0be9
Arthur Henrique de Vieira Serodio <a242810@dac.unicamp.br>
2026-03-23 00:31:24 +0000

    Teste

--- src/main/java/com/nexus/model/User.java
+++ src/main/java/com/nexus/model/User.java
@@ -1,12 +1,20 @@
 package com.nexus.model;
+import java.util.regex.Pattern;
+import com.nexus.service.Workspace;
 
 public class User {
     private final String username;
     private final String email;
+    private static String EMAIL_REGEX = "^[\\w.%+-]+@[\\w.-]+\\.[a-zA-Z]{2,}$";
+    private static Pattern EMAIL_PATTERN = Pattern.compile(EMAIL_REGEX);
+    private static Workspace workspace = new Workspace();
 
     public User(String username, String email) {
         if (username == null || username.isBlank()) {
             throw new IllegalArgumentException("Username não pode ser vazio.");
+        }
+        if (email == null || !EMAIL_PATTERN.matcher(email).matches()){
+            throw new IllegalArgumentException("E-mail Inválido");
         }
         this.username = username;
         this.email = email;
@@ -21,6 +29,12 @@ public class User {
     }
 
     public long calculateWorkload() {
-        return 0; 
+        int taskcount = 0;
+        for(Task task : User.workspace.getTasks()){
+            if (task.getOwner().consultUsername() == this.username && task.getStatus() == TaskStatus.IN_PROGRESS){
+                taskcount++;
+            }
+        }
+        return taskcount; 
     }
 }
